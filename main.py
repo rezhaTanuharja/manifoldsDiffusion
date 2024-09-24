122f0fc49d0f777a67803bc723a4e07ef22c395c
rezhaTanuharja <rezha.tanuharja@outlook.com>
2024-09-24 18:01:28 +0200

    Unfinished work

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ distribution = dm.distributions.InverseTransform(
     # cumulative_distribution_function = dm.distributions.functions.Heaviside(),
     distribution_function = dm.distributions.functions.periodic.HeatKernel(
         num_waves = 10,
-        mean_squared_displacement = lambda t: t
+        mean_squared_displacement = lambda t: 0.375 * t
     ),
     inversion_method = dm.distributions.inversion.Bisection(num_iterations = 10)
 )
@@ -21,10 +21,11 @@ distribution = dm.distributions.InverseTransform(
 angles = torch.pi * torch.arange(start = -1.0, end = 1.0, step = 0.01)
 # angles.to(device)
 
-times = [0.3 * (1.0 + i) for i in range(10)]
+times = [0.125 * (1.0 + i) for i in range(20)]
 
 for t in times:
-    values = distribution.at(time = t).cumulative_function()(angles)
+    # values = distribution.at(time = t).cumulative_function()(angles)
+    values = distribution.at(time = t).density_function()(angles)
     plt.plot(angles.cpu(), values.cpu())
 
 # random_samples = distribution.at(time = 1.0).sample(num_samples = 2000)
