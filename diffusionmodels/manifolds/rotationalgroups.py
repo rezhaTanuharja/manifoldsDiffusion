b1a52a9686cfb4ccfa5e44d70130ec80427356a1
rezhaTanuharja <rezha.tanuharja@outlook.com>
2024-11-26 10:24:58 +0100

    Converted SO3 implementation to use jax instead of torch

--- diffusionmodels/manifolds/rotationalgroups.py
+++ diffusionmodels/manifolds/rotationalgroups.py
@@ -1,31 +1,35 @@
 """
-manifolds.simpleclass
-=====================
+manifolds.rotationalgroups
+==========================
 
-Implements various simple Riemannian geometries
+Provides group structures of orthogonal matrices with determinant 1
 
 Classes
 -------
 SpecialOrthogonal3
-    The 3-dimension rotation matrix group
+    The 3-dimensional rotation matrix group
 """
 
 
-import torch
 from typing import Tuple
 
 from .interfaces import Manifold
 
+import jax
+import jax.numpy as jnp
+import jax.scipy as jsp
+
 
 class SpecialOrthogonal3(Manifold):
     """
-    The 3-dimension rotation matrix group.
+    The 3-dimensional rotation matrix group
     """
+    
 
     def __init__(self) -> None:
-
-        # the canonical basis of the tangent space at the identity element
-        self._bases = torch.Tensor([
+        
+        # canonical basis of tangent space at the identity element
+        self._bases = jnp.array([
             [
                 [ 0,  0,  0],
                 [ 0,  0, -1],
@@ -41,48 +45,61 @@ class SpecialOrthogonal3(Manifold):
                 [ 1,  0,  0],
                 [ 0,  0,  0]
             ]
-        ])
+        ], dtype = jnp.float32)
 
         self._dimension = (3, 3)
         self._tangent_dimension = (3,)
 
-    def to(self, device: torch.device) -> None:
-        self._bases = self._bases.to(device)
 
+    def to(self, device: jax.Device) -> None:
+        self._bases = jax.device_put(self._bases, device = device)
+
+    
     def dimension(self) -> Tuple[int, ...]:
         return self._dimension
+
 
     def tangent_dimension(self) -> Tuple[int, ...]:
         return self._tangent_dimension
 
-    def exp(
-        self,
-        points: torch.Tensor,
-        tangent_vectors: torch.Tensor
-    ) -> torch.Tensor:
 
-        # compute skew symmetric matrix from axis angle representation
-        skew_matrices = torch.einsum('...j, jkl -> ...kl', tangent_vectors, self._bases)
+    def exp(self, points: jnp.ndarray, vectors: jnp.ndarray) -> jnp.ndarray:
 
-        # parallel transport the curve and increment X
-        return torch.matmul(points, torch.linalg.matrix_exp(skew_matrices))
+        return jnp.matmul(
 
-    def log(self, origins: torch.Tensor, destinations: torch.Tensor) -> torch.Tensor:
+            points,
 
-        # compute inverse(destinations)origins
-        relative_rotation =  torch.einsum('...ji, ...jk -> ...ik', origins, destinations)
-
-        # compute geodesic distance, compensate for numerical inaccuracies
-        angle = torch.acos(
-            torch.clip(
-                0.5 * (torch.einsum('...ii -> ...', relative_rotation) - 1.0),
-                min = -1.0, max = 1.0
+            jsp.linalg.expm(
+                # the skew matrices associated with the vectors
+                jnp.einsum('...j, jkl -> ...kl', vectors, self._bases)
             )
+
         )
 
-        # compute axis of rotation
-        axis = torch.einsum('...ijk, ...jk -> ...i', self._bases, relative_rotation)
-        axis = axis / (torch.norm(axis, dim = -1, keepdim = True) + 1e-6)
+    
+    def log(self, starts: jnp.ndarray, ends: jnp.ndarray) -> jnp.ndarray:
 
-        # return the axis-angle representation
-        return torch.einsum('..., ...i -> ...i', angle, axis)
+        relative_rotation = jnp.matmul(starts.T, ends)
+
+        angle = jnp.arccos(
+
+            jnp.clip(
+
+                0.5 * jnp.trace(relative_rotation) - 1.0,
+
+                # clip for numerical stability
+                min = -1.0, max = 1.0
+
+            )
+
+        )
+
+        rotation_axis = jnp.einsum(
+            '...ijk, ...jk -> ...i', self._bases, relative_rotation
+        )
+
+        rotation_axis = rotation_axis / (
+            jnp.linalg.norm(rotation_axis, axis = -1, keepdims = True) + 1e-8
+        )
+
+        return jnp.einsum('..., ...i -> ...i', angle, rotation_axis)
