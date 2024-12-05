21f4876497972c3174f9ec0fec3a6ff4a4288715
rezhaTanuharja <rezha.tanuharja@outlook.com>
2024-12-05 14:56:39 +0100

    Fixed documentations for manifold interface

--- diffusionmodels/manifolds/interfaces.py
+++ diffusionmodels/manifolds/interfaces.py
@@ -7,12 +7,10 @@ Classes
 A purely abstract class that serves as an interface of all manifolds
 """
 
-
 from abc import ABC, abstractmethod
 from typing import Tuple
 
 import torch
-
 
 
 class Manifold(ABC):
@@ -24,24 +22,24 @@ class Manifold(ABC):
     `to(device)`
     Moves all tensor attributes to the given device
 
-    `dimension()`
-    Returns the shape of points on the manifold
-
-    `tangent_dimension()`
-    Returns the shape of vectors on the tangent space
-
     `exp(points, vectors)`
     Returns the results of incrementing the points by the vectors
 
     `log(starts, ends)`
     Returns the vectors such that `exp(starts, vectors) = ends`
-    """
 
+    Properties
+    ----------
+    `dimension`
+    The tensor shape of points on the manifold
+
+    `tangent_dimension`
+    The tensor shape of vectors on the tangent space
+    """
 
     @abstractmethod
     def __init__(self, *args, **kwargs) -> None:
         raise NotImplementedError("Subclasses must implement this method")
-
 
     @abstractmethod
     def to(self, device: torch.device) -> None:
@@ -51,32 +49,31 @@ class Manifold(ABC):
         Parameters
         ----------
         `device: torch.device`
-        A device object from Jax representing the target hardware
+        A device object representing the target hardware
         """
         raise NotImplementedError("Subclasses must implement this method")
 
-
+    @property
     @abstractmethod
     def dimension(self) -> Tuple[int, ...]:
         """
         Returns
         -------
         `Tuple[int, ...]`
-        The shape of points on the manifold, is always a tuple
+        The tensor shape of points on the manifold, is always a tuple
         """
         raise NotImplementedError("Subclasses must implement this method")
 
-
+    @property
     @abstractmethod
-    def tangent_dimension() -> Tuple[int, ...]:
+    def tangent_dimension(self) -> Tuple[int, ...]:
         """
         Returns
         -------
         `Tuple[int, ...]`
-        The shape of vectors on the tangent space, is always a tuple
+        The tensor shape of vectors on the tangent space, is always a tuple
         """
         raise NotImplementedError("Subclasses must implement this method")
-
 
     @abstractmethod
     def exp(self, points: torch.Tensor, vectors: torch.Tensor) -> torch.Tensor:
@@ -85,19 +82,18 @@ class Manifold(ABC):
 
         Parameters
         ----------
-        `points: jax.numpy.ndarray`
+        `points: torch.Tensor`
         Points on the manifold with shape `(..., *dimension)`
 
-        `vectors: jax.numpy.ndarray`
+        `vectors: torch.Tensor`
         Vectors on the tangent space with shape `(..., *tangent_dimension)`
 
         Returns
         -------
-        `jax.numpy.ndarray`
+        `torch.Tensor`
         Points on the manifold with shape `(..., *dimension)`
         """
         raise NotImplementedError("Subclasses must implement this method")
-
 
     @abstractmethod
     def log(self, starts: torch.Tensor, ends: torch.Tensor) -> torch.Tensor:
@@ -106,15 +102,15 @@ class Manifold(ABC):
 
         Parameters
         ----------
-        `starts: jax.numpy.ndarray`
+        `starts: torch.Tensor`
         Points on the manifold with shape `(..., *dimension)`
 
-        `ends: jax.numpy.ndarray`
+        `ends: torch.Tensor`
         Points on the manifold with shape `(..., *dimension)`
 
         Returns
         -------
-        `jax.numpy.ndarray`
+        `torch.Tensor`
         Vectors on the tangent space with shape `(..., *tangent_dimension)`
         """
         raise NotImplementedError("Subclasses must implement this method")
