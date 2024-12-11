f3e46271e037bfb36c06da709ebd2c590ddc6879
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2024-12-11 16:35:54 +0100

    Added documentation to univariate uniform distribution

--- diffusionmodels/stochasticprocesses/univariate/uniform.py
+++ diffusionmodels/stochasticprocesses/univariate/uniform.py
@@ -1,3 +1,12 @@
+"""
+Implements the univariate uniform probability density function.
+
+Classes
+-------
+`Uniform`
+A uniform probability density function defined on an interval
+"""
+
 from typing import Dict, Tuple
 
 import torch
@@ -7,9 +16,24 @@ from ..interfaces import DensityFunction
 
 
 class Uniform(DensityFunction):
+    """
+    A uniform probability density function defined on an interval
+    """
+
     def __init__(
         self, support: Dict[str, float], data_type: torch.dtype = torch.float32
     ) -> None:
+        """
+        Construct a density object for a univariate uniform distribution
+
+        Parameters
+        ----------
+        `support: Dict[str, float]`
+        A dictionary with keys 'lower' and 'upper', defines the interval
+
+        `data_type: torch.dtype = torch.float32`
+        The data type of floating points
+        """
         self._data_type = data_type
         self._lower = support["lower"]
         self._upper = support["upper"]
